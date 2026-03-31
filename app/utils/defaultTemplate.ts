c46bda79f959d7999da3863ef845b3c97b996111
Rasten Remizov <Rastuharem@gmail.com>
2026-03-31 13:13:01 +0300

    refactor: changed folders structure for default cv

--- app/utils/defaultTemplate.ts
+++ app/utils/defaultTemplate.ts
@@ -1,7 +1,7 @@
-import content from '@/assets/default-cv/content.yaml?raw'
-import template from '@/assets/default-cv/template.handlebars?raw'
-import styles from '@/assets/default-cv/styles.css?raw'
-import head from '@/assets/default-cv/head.html?raw'
+import content from '@/assets/templates/default/content.yaml?raw'
+import template from '@/assets/templates/default/template.handlebars?raw'
+import styles from '@/assets/templates/default/styles.css?raw'
+import head from '@/assets/templates/default/head.html?raw'
 
 export const defaultYaml = content
 export const defaultHbs = template
